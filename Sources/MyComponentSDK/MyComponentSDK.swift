bb509019cf83d2a0dcb5c165089e1ac27acc2826
verma mukesh <vermamukesh67@gmail.com>
2023-06-09 00:51:55 +0800

    2

--- Sources/MyComponentSDK/MyComponentSDK.swift
+++ Sources/MyComponentSDK/MyComponentSDK.swift
@@ -1,5 +1,8 @@
 import UIKit
 
 public struct MyComponentSDK {
-    public static let checkBoxImage = UIImage(named: "checkbox", in: Bundle.module, with: nil)
+    
+    public static let mbCheckBoxImage = UIImage(named: "checkbox", in: Bundle.module, with: nil)
+    
+    public static let bbCheckBoxImage = UIImage(named: "checkbox_bb", in: Bundle.module, with: nil)
 }
